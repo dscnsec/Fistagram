cbf493211301d276fc05e75c3bbf5ea2ab150b21
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-03-29 22:04:27 +0530

    implemented notification box widget

---
+++ lib/widgets/notification_box.dart
@@ -0,0 +1,104 @@
+import 'package:figma_squircle/figma_squircle.dart';
+import 'package:fistagram/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class NotificationBox extends StatefulWidget {
+  final String profilePicPath;
+  final String userId;
+  final String notificationMessage;
+  final String timePassed;
+  final bool isFollow;
+  final String postPic;
+  const NotificationBox({
+    Key? key, 
+    required this.profilePicPath, 
+    required this.userId, 
+    required this.notificationMessage, 
+    required this.timePassed,
+    required this.isFollow,
+    this.postPic="",
+  }) : super(key: key);
+
+  @override
+  State<NotificationBox> createState() => _NotificationBoxState();
+}
+
+class _NotificationBoxState extends State<NotificationBox> {
+
+  Widget followButton(){
+    return Container();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 20,
+            child: ClipOval(child: Image.asset(widget.profilePicPath)) ,
+            backgroundColor: Colors.transparent,
+          ),
+          const SizedBox( width: 10),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text( widget.userId, 
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ) 
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text( widget.notificationMessage,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300
+                      ) 
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text( widget.timePassed,
+                      style:  const TextStyle(
+                        color:  primaryShadowColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500
+                      )
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          if(widget.isFollow)
+            followButton()
+          else
+             ClipSmoothRect(
+               radius: SmoothBorderRadius(
+                 cornerRadius: 5, 
+                 cornerSmoothing: 1,
+               ),
+               child: Container(
+                 width: 40,
+                 height: 40,
+                 child: Image.asset(
+                    widget.postPic,
+                    fit: BoxFit.cover
+                   )
+                 )
+               )
+        ]
+      ),
+    );
+  }
+}
