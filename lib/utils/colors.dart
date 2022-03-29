14ef10bebf44707cc00b7d365af724b9cf9ad084
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-03-29 22:03:43 +0530

    added a shadow text color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const backgroundColor= Color.fromARGB(255, 239, 243, 245);
 const secondaryBackgroundColor= Color.fromARGB(245, 229, 236, 240);
 const primaryColor= Color.fromARGB(255, 60, 97, 112);
+const primaryShadowColor = Color.fromARGB(255, 149, 167, 177);
