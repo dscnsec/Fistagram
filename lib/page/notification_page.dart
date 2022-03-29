d33ad91e40c2a6e81cb02bdb0f82d95b872659de
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-03-29 20:38:14 +0530

    implemented appbar in notification page

--- lib/page/notification_page.dart
+++ lib/page/notification_page.dart
@@ -1,3 +1,4 @@
+import 'package:fistagram/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class NotificationPage extends StatefulWidget {
@@ -10,10 +11,31 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-   return SingleChildScrollView(
-      child: SafeArea(child: Container(
-        alignment: Alignment.center, 
-        child: Text('Notification Page'),)),
-    ); 
+   return Scaffold(
+     backgroundColor: backgroundColor,
+     appBar: AppBar(
+       centerTitle: true,
+       toolbarHeight: 60,
+       title: const Text('Notifications', 
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w300,
+          ),
+       ),
+       backgroundColor: backgroundColor,
+       elevation: 0,
+     ),
+     body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+          padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+          alignment: Alignment.center, 
+          child: Column(
+            children: [],
+          )
+          )
+        ),
+      ),
+   ); 
   }
 }
