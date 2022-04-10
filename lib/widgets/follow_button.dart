8b2bf07cd1074ee489b27efc445af07c71fd8c4e
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-04-11 01:31:20 +0530

    Implemented follow button widget

---
+++ lib/widgets/follow_button.dart
@@ -0,0 +1,39 @@
+import 'package:figma_squircle/figma_squircle.dart';
+import 'package:flutter/material.dart';
+
+
+class FollowButton extends StatefulWidget {
+  const FollowButton({Key? key}) : super(key: key);
+
+  @override
+  State<FollowButton> createState() => _FollowButtonState();
+}
+
+class _FollowButtonState extends State<FollowButton> {
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: (){},
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 65, 184, 244)),
+        fixedSize: MaterialStateProperty.all(const Size(50, 20)),
+        minimumSize: MaterialStateProperty.all(const Size(70, 30)),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: SmoothBorderRadius(
+              cornerRadius: 5,
+              cornerSmoothing: 1
+          )
+          ) 
+        )),
+      child: const Text('Follow', 
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 14,
+          fontWeight: FontWeight.w400
+        )
+        )
+    );
+  }
+}
