4b93b2949035c667757311235480d1dfeb70e384
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-05-01 19:50:25 +0530

    added primary Input box color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -5,3 +5,4 @@ const secondaryBackgroundColor= Color.fromARGB(245, 229, 236, 240);
 const primaryColor= Color.fromARGB(255, 60, 97, 112);
 const primaryShadowColor = Color.fromARGB(255, 149, 167, 177);
 const primaryButtonColor = Color.fromARGB(255, 65, 184, 244);
+const primaryInputBoxColor = Color.fromARGB(255, 209, 226, 236);
