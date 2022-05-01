f83c89ebfe9dcccf2ae35444000c73b898fcbb48
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-05-01 14:02:21 +0530

    added primary button color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,3 +4,4 @@ const backgroundColor= Color.fromARGB(255, 239, 243, 245);
 const secondaryBackgroundColor= Color.fromARGB(245, 229, 236, 240);
 const primaryColor= Color.fromARGB(255, 60, 97, 112);
 const primaryShadowColor = Color.fromARGB(255, 149, 167, 177);
+const primaryButtonColor = Color.fromARGB(255, 65, 184, 244);
