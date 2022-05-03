39b7a2e9c0e1d94fe33a916d5af857b9d48550ce
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-05-03 20:27:14 +0530

    basic signup process implemented
    Signed-off-by: SP-XD <69642436+SP-XD@users.noreply.github.com>

--- lib/page/signup_page.dart
+++ lib/page/signup_page.dart
@@ -1,4 +1,5 @@
 import 'package:figma_squircle/figma_squircle.dart';
+import 'package:fistagram/resources/auth_methods.dart';
 import 'package:fistagram/utils/colors.dart';
 import 'package:fistagram/widgets/text_field_input.dart';
 import 'package:flutter/material.dart';
@@ -11,45 +12,45 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
-  
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-   final TextEditingController _emailController= TextEditingController();
-   final TextEditingController _passwordController= TextEditingController();
-   final TextEditingController _bioController = TextEditingController();
-   final TextEditingController _usernameController = TextEditingController();
-   @override
-   void dispose(){
-     super.dispose();
-     _emailController.dispose();
-     _passwordController.dispose();
-     _bioController.dispose();
-     _usernameController.dispose();
-   }
-
-   return Scaffold(
-     backgroundColor: backgroundColor,
+    return Scaffold(
+      backgroundColor: backgroundColor,
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
         child: SafeArea(
           child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Flexible(child: Container(), flex: 1),
-                //png Image,
-                SizedBox(
-                  height: 64,
-                  child: Image.asset('assets/img/fistagram_logo.png', color: primaryColor,)
-                ),
-                const SizedBox( height: 24),
-                //profile pic
-                Stack(
-                  children: [
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Flexible(child: Container(), flex: 1),
+                  //png Image,
+                  SizedBox(
+                      height: 64,
+                      child: Image.asset(
+                        'assets/img/fistagram_logo.png',
+                        color: primaryColor,
+                      )),
+                  const SizedBox(height: 24),
+                  //profile pic
+                  Stack(children: [
                     const CircleAvatar(
                       radius: 64,
                       backgroundImage: NetworkImage(
@@ -60,80 +61,100 @@ class _SignupPageState extends State<SignupPage> {
                       bottom: -10,
                       left: 80,
                       child: IconButton(
-                        onPressed: (){}, 
-                        icon: const Icon(
-                          Icons.add_a_photo, 
-                          color: primaryColor,
-                        )),
-                      )
-                  ]
-                ),
-                const SizedBox( height: 24),
-                //text field for username 
-                TextFieldInput(textEditingController: _usernameController, iconType: Icons.portrait_rounded, hintText: 'Enter your username...', textInputType: TextInputType.text),
-                const SizedBox( height: 24),
-                //text field for bio
-                TextFieldInput(textEditingController: _bioController, iconType: Icons.text_fields_outlined, hintText: 'Enter your bio...', textInputType: TextInputType.text),
-                const SizedBox( height: 24),
-                //text field for email
-                TextFieldInput(textEditingController: _emailController,iconType: Icons.email_outlined, hintText: 'Enter your email...', textInputType: TextInputType.emailAddress),
-                //text field for password
-                const SizedBox( height: 24),
-                TextFieldInput(textEditingController: _passwordController, iconType: Icons.lock_clock_outlined,hintText: 'Enter your password...', textInputType: TextInputType.text, isObscure: true,),
-                // button login 
-                const SizedBox( height: 24),
-                InkWell(
-                  onTap: (){},
-                  child: Container(
-                    child: const Text('Sign up', 
-                      style: TextStyle(
-                        color: Colors.white
-                      ),
-                    ),
-                    width: double.infinity,
-                    height: 44,
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    decoration: ShapeDecoration(shape: RoundedRectangleBorder(
-                      borderRadius: SmoothBorderRadius(
-                        cornerRadius: 6,
-                        cornerSmoothing: 1,
-                      ),
-                    ),
-                    color: primaryButtonColor),
-                  ),
-                ),
-                const SizedBox( height: 14),
-                Flexible(child: Container(), flex: 1),
-                Row (
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      child: const Text("Already have an account?",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w300
-                        )
-                      ),
-                      padding: const EdgeInsets.symmetric( vertical: 8),
-                    ),
-                    GestureDetector(
-                      onTap: (){},
-                      child: Container(
-                        child: const Text("Log in.",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 8)
-                        ),
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.add_a_photo,
+                            color: primaryColor,
+                          )),
                     )
-                  ],
-                ),
-                ],) 
-              ),
-            ),
+                  ]),
+                  const SizedBox(height: 24),
+                  //text field for username
+                  TextFieldInput(
+                      textEditingController: _usernameController,
+                      iconType: Icons.portrait_rounded,
+                      hintText: 'Enter your username...',
+                      textInputType: TextInputType.text),
+                  const SizedBox(height: 24),
+                  //text field for bio
+                  TextFieldInput(
+                      textEditingController: _bioController,
+                      iconType: Icons.text_fields_outlined,
+                      hintText: 'Enter your bio...',
+                      textInputType: TextInputType.text),
+                  const SizedBox(height: 24),
+                  //text field for email
+                  TextFieldInput(
+                      textEditingController: _emailController,
+                      iconType: Icons.email_outlined,
+                      hintText: 'Enter your email...',
+                      textInputType: TextInputType.emailAddress),
+                  //text field for password
+                  const SizedBox(height: 24),
+                  TextFieldInput(
+                    textEditingController: _passwordController,
+                    iconType: Icons.lock_clock_outlined,
+                    hintText: 'Enter your password...',
+                    textInputType: TextInputType.text,
+                    isObscure: true,
+                  ),
+                  // button login
+                  const SizedBox(height: 24),
+                  InkWell(
+                    onTap: () async {
+                      String res = await AuthMethods().signUpUser(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                          username: _usernameController.text,
+                          bio: _bioController.text);
+                      print(res);
+                    },
+                    child: Container(
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      width: double.infinity,
+                      height: 44,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: SmoothBorderRadius(
+                              cornerRadius: 6,
+                              cornerSmoothing: 1,
+                            ),
+                          ),
+                          color: primaryButtonColor),
+                    ),
+                  ),
+                  const SizedBox(height: 14),
+                  Flexible(child: Container(), flex: 1),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: const Text("Already have an account?",
+                            style: TextStyle(fontWeight: FontWeight.w300)),
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                            child: const Text(
+                              "Log in.",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 8)),
+                      )
+                    ],
+                  ),
+                ],
+              )),
+        ),
       ),
-        );
-    }
+    );
   }
-
+}
---
+++ lib/resources/auth_methods.dart
@@ -0,0 +1,48 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    //required Uint8List file,
+  }) async {
+    String res = "Some error occurred";
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty) {
+        //register user
+        UserCredential credential = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        print("user uid"+credential.user!.uid);
+
+        //add usr to our database
+        _firestore.collection('user').doc(credential.user!.uid).set({
+          'username': username,
+          'uid': credential.user!.uid,
+          'email': email,
+          'bio': bio,
+          'followers': [],
+          'following': [],
+        });
+
+        res = "success";
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+
+    return res;
+  }
+}
