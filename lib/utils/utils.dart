9547522c0ecfe110ebeb8364fa79f31eae769f81
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-05-09 21:09:53 +0530

    implemented snackbar util

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -12,3 +12,9 @@ pickImage(ImageSource source) async {
 
   print('No image selected');
 }
+
+ShowSnackBar(String content, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text(content)),
+  );
+}
